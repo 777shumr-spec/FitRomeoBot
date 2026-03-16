1a2d89b92ba5eaf209d78f1555d64ce9489be7b1
777shumr-spec <777shum.r@gmail.com>
2026-03-16 09:59:19 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers_user import router as user_router
+
+
+async def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(user_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
